f0ddf8a4e2d5e76fcc2645cc5966c6c83f32af9a
Bastien Decorte <seriousbooster@gmail.com>
2019-12-11 14:04:05 +0100

    Added the header.

--- app/src/main/java/com/example/cuisinhelha/HomeActivity.java
+++ app/src/main/java/com/example/cuisinhelha/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.cuisinhelha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class HomeActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/cuisinhelha/MainActivity.java
+++ app/src/main/java/com/example/cuisinhelha/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.cuisinhelha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void test(View v){
+        Toast.makeText(MainActivity.this, "Well", Toast.LENGTH_SHORT).show();
     }
 }
